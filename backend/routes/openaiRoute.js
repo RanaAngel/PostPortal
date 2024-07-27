e8dca71010ef29d9166f6e24865d4014536c86fe
RanaAngel <angel.rana@apexcollege.edu.np>
2024-07-28 02:06:01 +0545

    Diagnose
    test

--- backend/routes/openaiRoute.js
+++ backend/routes/openaiRoute.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 // Access your API key as an environment variable
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+const genAI = new GoogleGenerativeAI(AIzaSyBnRREf6yfjLdu9XGDjGwk7yDdQWj24POY);
 const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
 
 router.post('/generate-post', async (req, res) => {
   const { title } = req.body;
+  console.log(title);
 
   try {
     const result = await model.generateContent(`Create a post about "${title}" that includes the following: A catchy headline, Key benefits of the topic, and A call-to-action at the end in a paragraph format.Also do not exceed 228 characters`);
