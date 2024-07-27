f0b435629a652752ec8a2ead0e6b019ddefdf1af
RanaAngel <angel.rana@apexcollege.edu.np>
2024-07-28 02:09:13 +0545

    Final
    Tested

--- backend/routes/openaiRoute.js
+++ backend/routes/openaiRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 // Access your API key as an environment variable
-const genAI = new GoogleGenerativeAI();
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
 
 router.post('/generate-post', async (req, res) => {
